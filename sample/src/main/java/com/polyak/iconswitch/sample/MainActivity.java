959fc174c6ad45c9476b7681cd4203de41ed4bcd
polyak01 <polyakov.production@gmail.com>
2017-03-30 23:34:30 +0300

    initialized repository

---
+++ sample/src/main/java/com/polyak/iconswitch/sample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.polyak.iconswitch.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.polyak.iconswitch.R.layout.activity_main);
+    }
+}
